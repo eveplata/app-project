af23c5e4332b47c0d449d1cb765f630ae81b82a7
Evelyn <platacanavirie@gmail.com>
2023-12-02 15:34:54 -0400

    reclamos2

--- src/app/pages/reclamos/reclamos.page.ts
+++ src/app/pages/reclamos/reclamos.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { ReclamosService } from 'src/app/services/reclamos.service';
 
 @Component({
   selector: 'app-reclamos',
@@ -7,14 +8,23 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./reclamos.page.scss'],
 })
 export class ReclamosPage implements OnInit {
+  reclamos: any[] = [];
 
   constructor(
     private navCtrl: NavController,
+    private reclamosService: ReclamosService,
 
   ) { }
 
   ngOnInit() {
+    this.obtenerCategorias();
   }
+  obtenerCategorias() {
+    this.reclamosService.getObetenerReclamos().subscribe((resp) => {
+      this.reclamos = resp;
+    });
+  }
+  
 
   backToPage() {
     this.navCtrl.navigateForward('home');
